8ee41db1dd18a5207ed74b8b25431db4853e61b7
kokoball8686 <kokoball8686@gmail.com>
2024-12-31 14:16:47 +0900

    3

--- django-apps/myapp/urls.py
+++ django-apps/myapp/urls.py
@@ -3,6 +3,8 @@ from myapp import views
 
 urlpatterns = [
     path('', views.index),
-
-
+    path('create/', views.create),
+    path('read/<id>/', views.read),
+    path('update/<id>', views.update),
+    path('delete/', views.delete)
 ]
